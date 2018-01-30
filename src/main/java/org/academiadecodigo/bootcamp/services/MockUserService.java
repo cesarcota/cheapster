51310b40c1985d0399cff432e1972efe2c600eac
Luis Serpa <luis.ff.serpa@hotmail.com>
2018-01-30 20:40:51 -0100

    user service ready

---
+++ src/main/java/org/academiadecodigo/bootcamp/services/MockUserService.java
@@ -0,0 +1,56 @@
+package org.academiadecodigo.bootcamp.services;
+
+
+import org.academiadecodigo.bootcamp.models.Group;
+import org.academiadecodigo.bootcamp.models.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MockUserService implements UserService{
+
+    private Map<String,User> userMap = new HashMap<>();
+
+    @Override
+    public void addUser(User user) {
+
+        if(user==null){
+            return;
+        }
+
+        userMap.put(user.getMail(),user);
+
+    }
+
+    @Override
+    public void removeUser(User user) {
+
+        if(user==null){
+            return;
+        }
+
+        userMap.remove(user.getMail());
+
+    }
+
+    @Override
+    public User findByEmail(String email) {
+
+        return userMap.get(email);
+
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+
+        return new ArrayList<User>(userMap.values());
+    }
+
+    @Override
+    public List<Group> findAllGroups(User user) {
+
+        return new ArrayList<Group>(user.getGroupMap().values());
+    }
+}
